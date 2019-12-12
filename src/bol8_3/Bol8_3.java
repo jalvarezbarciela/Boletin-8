1d248b7d5b71922bef76ccb38e3e7fd5682545c9
jalvarezbarciela <56076790+jalvarezbarciela@users.noreply.github.com>
2019-12-12 14:24:30 +0100

    Add files via upload

---
+++ src/bol8_3/Bol8_3.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bol8_3;
+
+/**
+ *
+ * @author Jose
+ */
+public class Bol8_3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Meses a=new Meses();
+       a.diasSegunMeses();
+    }
+    
+}
